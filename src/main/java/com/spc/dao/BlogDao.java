3ff3cc1a7b4fcdfc10083b6b7704e31fcbd7a67b
spc <1124516293@qq.com>
2020-04-26 17:00:09 +0800

    TDD blog's model development code

--- src/main/java/com/spc/dao/BlogDao.java
+++ src/main/java/com/spc/dao/BlogDao.java
@@ -1,9 +1,10 @@
 package com.spc.dao;
 
 import com.spc.entity.Blog;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class BlogDao {
     public List<Blog> getBlogs(Integer page, Integer pagesize, Integer userId) {
         return null;
